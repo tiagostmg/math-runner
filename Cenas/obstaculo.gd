1435d631e107f9fb897898c05699265eda810660
tiagostmg <tiago.taumaturgo@gmail.com>
2025-04-26 11:54:46 -0300

    Adição das perguntas e respostas de Conjuntos, PA e PG

--- Cenas/obstaculo.gd
+++ Cenas/obstaculo.gd
@@ -40,19 +40,19 @@ func _ready() -> void:
 
 func _on_timer_timeout() -> void:
 	_mudarpos()
-	if Global.pontuacao == 1000 and Global.fase==1:
+	if Global.pontuacao == 500 and Global.fase==1:
 		$"../Linha Chegada".position.y = $".".position.y
 		$".".visible = false
 		$"../Fundo Obstaculo".visible = false
 		$"../Camera2D/Label".visible = false
 		
-	elif Global.pontuacao == 1000 and Global.fase==2:
+	elif Global.pontuacao == 500 and Global.fase==2:
 		$"../Linha Chegada".position.y = $".".position.y
 		$".".visible = false
 		$"../Fundo Obstaculo".visible = false
 		$"../Camera2D/Label".visible = false
 		
-	elif Global.pontuacao == 1000 and Global.fase==3:
+	elif Global.pontuacao == 500 and Global.fase==3:
 		$"../Linha Chegada".position.y = $".".position.y
 		$".".visible = false
 		$"../Fundo Obstaculo".visible = false
@@ -67,6 +67,174 @@ func _mudarpos():
 func sortear(a):
 	
 	# a -> fase
+	
+	var questoesConjuntos = [
+		{
+			"pergunta": "Se A ⊆ B, isso significa que:",
+			"opcoes": [
+				"Todo elemento de A está em B.",
+				"Todo elemento de B está em A.",
+				"A e B são conjuntos disjuntos."
+			],
+			"resposta_correta": 0
+		},
+		{
+			"pergunta": "Qual é a união dos conjuntos A = {1, 2} e B = {2, 3}?",
+			"opcoes": [
+				"{1, 2}",
+				"{1, 2, 3}",
+				"{2, 3}"
+			],
+			"resposta_correta": 1
+		},
+		{
+			"pergunta": "Se A = {1, 2, 3} e B = {2, 3, 4}, qual é a interseção de A e B?",
+			"opcoes": [
+				"{1}",
+				"{2, 3}",
+				"{3, 4}"
+			],
+			"resposta_correta": 1
+		},
+		{
+			"pergunta": "Qual das alternativas representa a união de dois conjuntos A={2,4} e B={1,4,5}?",
+			"opcoes": [
+				"{2,4,1}",
+				"{1,2,4,5}",
+				"{4}"
+			],
+			"resposta_correta": 1
+		},
+		{
+			"pergunta": "Se A={1,2,3}, qual é o complemento de A considerando o universo U={1,2,3,4,5}",
+			"opcoes": [
+				"{4,5}",
+				"{1,2,3}",
+				"{3,4}"
+			],
+			"resposta_correta": 0
+		},
+		{
+			"pergunta": "Se A={1,2,3} e B={3,4,5}, qual é a interseção de A e B?",
+			"opcoes": [
+				"{1,2}",
+				"{3}",
+				"{1,2,3,4,5}"
+			],
+			"resposta_correta": 1
+		},
+		{
+			"pergunta": "Qual operação entre dois conjuntos resulta em um novo conjunto contendo apenas os elementos comuns entre os dois conjuntos?",
+			"opcoes": [
+				"União",
+				"Interseção",
+				"Complemento"
+			],
+			"resposta_correta": 1
+		},
+		{
+			"pergunta": "Em qual conjunto numérico o número -2,5 está inserido?",
+			"opcoes": [
+				"Inteiros(Z)",
+				"Naturais(N)",
+				"Racionais(Q)"
+			],
+			"resposta_correta": 2
+		},
+		{
+			"pergunta": "O número √2 pertence a quais conjuntos?",
+			"opcoes": [
+				"Racionais(Q)",
+				"Irracionais(I)",
+				"Inteiros(Z)"
+			],
+			"resposta_correta": 1
+		},
+		{
+			"pergunta": "O conjunto ∅ (vazio) é subconjunto de quais conjuntos?",
+			"opcoes": [
+				"Apenas dos Naturais(N)",
+				"De qualquer conjunto",
+				"De nenhum conjunto"
+			],
+			"resposta_correta": 1
+		},
+		{
+			"pergunta": "",
+			"opcoes": [
+				"",
+				"",
+				""
+			],
+			"resposta_correta": 0
+		},
+		{
+			"pergunta": "",
+			"opcoes": [
+				"",
+				"",
+				""
+			],
+			"resposta_correta": 0
+		}
+	]
+
+	var questoesPaPg = [
+		{
+			"pergunta": "Qual é a razão da PA 5, 8, 11, 14, …?",
+			"opcoes": ["2", "3", "5"],
+			"resposta_correta": 1
+		},
+		{
+			"pergunta": "Qual é o 5º termo da PA 2, 6, 10, 14, …?",
+			"opcoes": ["18", "22", "26"],
+			"resposta_correta": 1
+		},
+		{
+			"pergunta": "Em uma PA, a1 = 7 e r = -2. Qual é o 4º termo?",
+			"opcoes": ["1", "3", "5"],
+			"resposta_correta": 0
+		},
+		{
+			"pergunta": "Se o primeiro termo de uma PA é 3 e a razão é 4, qual é o 6º termo?",
+			"opcoes": ["19", "23", "27"],
+			"resposta_correta": 1
+		},
+		{
+			"pergunta": "A sequência 10, 7, 4, 1, … é uma PA de razão:",
+			"opcoes": ["-2", "-3", "-4"],
+			"resposta_correta": 1
+		},
+		{
+			"pergunta": "Qual é a soma dos cinco primeiros termos da PA 1, 4, 7, 10, …?",
+			"opcoes": ["25", "35", "45"],
+			"resposta_correta": 1
+		},
+		{
+			"pergunta": "Em uma PA de razão 5, o 1º termo é 2. Qual é o 7º termo?",
+			"opcoes": ["27", "32", "37"],
+			"resposta_correta": 1
+		},
+		{
+			"pergunta": "A diferença entre dois termos consecutivos de uma PA é chamada de:",
+			"opcoes": ["Termo geral", "Razão", "Soma dos termos"],
+			"resposta_correta": 1
+		},
+		{
+			"pergunta": "Em uma PA, se a1 = 0 e r = 6, qual é o 8º termo?",
+			"opcoes": ["42", "48", "54"],
+			"resposta_correta": 0
+		},
+		{
+			"pergunta": "Na PA 15, 12, 9, 6, …, qual é o valor da razão?",
+			"opcoes": ["-2", "-3", "-6"],
+			"resposta_correta": 1
+		}
+	]
+
+
+
+
 	
 	var num1 = 0
 	var num2 = 0
